9443547a48df87f16249823226f4f17aaa3a7785
[hannahtong7] <[s2743332@ed.ac.uk]>
2024-11-29 12:03:52 +0000

    Update to vertebrate script

---
+++ Scripts/Vertebrates - field.R
@@ -0,0 +1,3 @@
+#All data compiled gives a more complete picture of the invertebrate communities on each hillside.
+#Aims to provide a overall diversity and richness as a single metric, which may be more meaningful for restoration recommendations.
+#Each method captures a different subset of the community (e.g., nocturnal, aquatic, terrestrial), and combining them helps capture the full spectrum of invertebrate diversity.
